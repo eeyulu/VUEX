06cab16e9c81bd2347f6152fda74bbf09ed59430
eeyulu <kqoo@163.com>
2018-01-25 16:20:25 +0800

    VUEX

---
+++ src/vuex/store.js
@@ -0,0 +1,53 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+const state={
+    count:6
+}
+
+const mutations={
+    add(state,n){
+        state.count+=n;
+    },
+    reduce(state){
+        state.count--;
+    }
+}
+
+const getters={
+    count:function(state){
+        return state.count+=100;
+    }
+}
+
+const actions={
+    addActions(context){
+        context.commit('add',10);
+        setTimeout(() => {
+            context.commit('reduce')            
+        }, 5000);
+        console.log('我经reduce先执行了');
+    },
+    reduceActions({commit}){
+        commit('reduce');
+    }
+
+}
+
+// const moduleA={
+//     state,
+//     mutations,
+//     getters,
+//     actions
+// }
+
+export default new Vuex.Store({
+    state,
+    mutations,
+    getters,
+    actions
+})
+// export default new Vuex.Store({
+//     modules:{a:moduleA}
+// })
